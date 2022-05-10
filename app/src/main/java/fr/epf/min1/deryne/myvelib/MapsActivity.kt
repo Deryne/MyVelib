c6ea37b73c44ede6b7388953d77aba062d4f565b
deryn <deryne.four@epfedu.fr>
2022-05-10 17:18:33 +0200

    Premier commit
    L'api récupère le json

---
+++ app/src/main/java/fr/epf/min1/deryne/myvelib/MapsActivity.kt
@@ -0,0 +1,99 @@
+package fr.epf.min1.deryne.myvelib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import fr.epf.min1.deryne.myvelib.databinding.ActivityMapsBinding
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val TAG = "MapsActivity"
+val stations : MutableList<Station> =  mutableListOf() //liste vide //
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+
+
+        val service =  retrofit.create(Velibs::class.java)
+
+        runBlocking{//ne bloque pas
+            val result = service.getUsers()
+            Log.d(TAG, "synchroAPI: ${result.data.stations}")
+            /*val users = result.results
+            users.map{
+                Station(it.capacity, it.lat, it.lon, it.name, it.rental_methods, it.stationCode, it.station_id)
+            }
+                .map{
+                    stations.add(it)
+                    val station = LatLng(it.lat, it.lon)
+                    mMap.addMarker(MarkerOptions().position(station).title("Marker"))
+
+
+                }*/
+        }
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/fr/epf/min1/deryne/myvelib/Station.kt
@@ -0,0 +1,10 @@
+package fr.epf.min1.deryne.myvelib
+
+data class Station(
+    val capacity: Int,
+    val lat: Double,
+    val lon: Double,
+    val name: String,
+    val stationCode: String,
+    val station_id: Long
+)
