93327909ed15775dd3058535ad2b27843a2b1dae
Valentinemrct <100784265+Valentinemrct@users.noreply.github.com>
2022-05-11 15:58:04 +0200

    List favoris
    reste à paufiner la liste + input

---
+++ app/src/main/java/fr/epf/min1/deryne/myvelib/FavListActivity.kt
@@ -0,0 +1,35 @@
+package fr.epf.min1.deryne.myvelib
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import fr.epf.min1.deryne.myvelib.databinding.ActivityFavListBinding
+
+class FavListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFavListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityFavListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_fav_list)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
